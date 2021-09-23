6352192a9f41a36db7a43cdb7c48c35409ca7534
sarahdzhu <zhudo@ssfs.org>
2021-09-23 14:48:00 -0400

    Points and Rectangle Practice

--- src/PointsAndRectangles.java
+++ src/PointsAndRectangles.java
@@ -1,15 +1,43 @@
-import java.awt.Rectangle;
-import java.awt.Point;
-import java.awt.Dimension;
+import java.awt.*;
 import java.util.Scanner;
 
-public class PointsAndRectangles
-{
+public class PointsAndRectangles {
 
-    public static void main(String[] args)
-    {
-        Scanner kb = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // create print statements and get input for the x and y values.
+        System.out.print("Enter the x coordinate: ");
+        int x = scan.nextInt();
+        System.out.print("Enter the y coordinate: ");
+        int y = scan.nextInt();
+        Point p = new Point(x, y);
+
+
+
+        System.out.print("Enter the length: ");
+        int width = scan.nextInt();
+        System.out.print("Enter the width: ");
+        int height = scan.nextInt();
+        Dimension d = new Dimension(width, height);
+        Rectangle r = new Rectangle(x,y,width,height);
+        System.out.println(p);
+        System.out.println(d);
+        System.out.println(r);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //Instantiate a Point object p. Hint: you will need the keyword "new"
 
