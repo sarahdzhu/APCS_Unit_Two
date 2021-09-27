2fee1aae0667a5f655e3d13bc95e3fb65b2e285b
sarahdzhu <zhudo@ssfs.org>
2021-09-27 08:48:19 -0400

    Fight Song

--- src/FightSong.java
+++ src/FightSong.java
@@ -5,15 +5,41 @@ public class FightSong {
     It is also void, which means it does not return any information. It should be marked private so that it can
     only be used in this class. Since I need to access it for testing purposes, it must be public.
      */
-    public static void goTeam() {
-        // Add any print statements you think should go in this method.
+    public static void justOnce() {
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
 
     }
+    public static void goTeam() {
+        // Add any print statements you think should go in this method.
+System.out.println("Go, team, go!");
+System.out.println("You can do it.");
+    }
+public static void space(){
+        System.out.println("");
 
+}
     // Create any additional methods you need in this space in order to fulfill the requirements of the program
+
+
 
     public static void main(String[] args) {
         // No print statements should be in here, only method calls.
+        goTeam();
+        space();
+        goTeam();
+        justOnce();
+        goTeam();
+        space();
+        goTeam();
+        justOnce();
+        goTeam();
+        space();
+        goTeam();
+
+
+
+
 
     }
 }
