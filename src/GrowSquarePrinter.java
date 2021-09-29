df5d17fc7cc27b99f8046ddacb47b947bd55ee98
sarahdzhu <zhudo@ssfs.org>
2021-09-29 11:06:40 -0400

    Rectangle Fun

--- src/GrowSquarePrinter.java
+++ src/GrowSquarePrinter.java
@@ -5,12 +5,18 @@ public class GrowSquarePrinter {
     public static void main(String[] args) {
 
         // Create a Rectangle object with a starting x, y of (100, 100) and a width and height of 50.
+Rectangle r = new Rectangle(100,100,50,50 );
 
         // print the rectangle object.
+        System.out.println(r);
+
 
         // Use the translate and grow methods as described in the instructions
+        r.translate(25,25);
+        r.grow(25,25);
 
         // print the rectangle object.
+        System.out.println(r);
 
     }
 }
--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -4,16 +4,31 @@ import java.util.Scanner;
 public class RectangleFun {
 
     public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
         // Create a scanner object to be able to get input from the user
-        Scanner scan = new Scanner(System.in);
-
+        System.out.print("X: ");
+        int x = scan.nextInt();
+        System.out.print("Y: ");
+        int y = scan.nextInt();
+        System.out.print("Width: ");
+        int width =scan. nextInt();
+        System.out.print("Height: ");
+        int height= scan.nextInt();
         // Get input for x, y, width, and height. Create four separate variables for these.
-
-
         // Create a Rectangle object using the four pieces of user input.
-
+        Rectangle r= new Rectangle(x,y,width,height);
 
         // Use the appropriate methods (getWidth() and getHeight() ) to calculate the perimeter, then print it
+        double perimeter =(2*r.getHeight()+2*r.getWidth());
+        System.out.println("Perimeter = "+perimeter);
+         r.setLocation ((int)(r.getX())-4,(int)(r.getY()-2));
+
+        System.out.println("New location is ("+r.getX()+", "+r.getY()+")" );
+
+
+
+
 
         // Use getX(), getY(), and setLocation() to move the Rectangle. Print the new location.
 
