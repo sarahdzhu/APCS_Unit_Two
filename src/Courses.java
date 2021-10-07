f21283a6f4733d0f569170ab303f15c3d08769aa
sarahdzhu <zhudo@ssfs.org>
2021-10-07 09:00:35 -0400

    Courses

--- src/Courses.java
+++ src/Courses.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
 public class Courses {
-}
+    public static void main(String[] args) {
+    Scanner scan= new Scanner(System.in);
+
+    System.out.print("What is the description of the course? ");
+    String description= scan.nextLine();
+
+    int des= description.indexOf(" ");
+
+    description.indexOf(" ", des+1);
+
+
+
+
+
+    System.out.println("Department: "+ description.substring(0,(description.indexOf(" ")+1)));
+    System.out.print("Course Number: "+ description.substring(description.indexOf(" ", des+1)));
+
+
+
+
+}}
