54f3a5ed5f47de63296c351bcfceb4888ff4a55f
sarahdzhu <zhudo@ssfs.org>
2021-10-11 19:04:06 -0400

    Wrapper Class

--- src/WrappersDelight.java
+++ src/WrappersDelight.java
@@ -1,7 +1,30 @@
 public class WrappersDelight {
+    public static void main(String[] args) {
 
     // Create a main class and follow the instructions on the Lab sheet to complete this assignment.
     // When finished, uncomment the WrappersDelight.main(null) line in the
     // test file to make sure your program is correct.
+    int num1 = 14;
+    Integer num2 = 8;
+    double num3 = 2.5;
+    Double num4 = num2+num3;
+    Integer two=num2-3;
+    Double three =num1+two+num4;
+    double one= two+three;
 
+        System.out.println(num1);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(num4);
+
+
+
+
+
+
+
+
+
+
+ }
 }
